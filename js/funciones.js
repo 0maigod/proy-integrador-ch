5d1462c45cfd41e9b7b75970dce78bc4358bc346
0maigod <easterisco@gmail.com>
2020-09-08 18:17:14 -0300

    Maquetado Clase Carrito y Modal del Carrito

--- js/funciones.js
+++ js/funciones.js
@@ -8,19 +8,22 @@ class Item {
   }
 }
 
-// class Cart {
-//   constructor() {
-//     this.products = [];
-//   }
+class Carrito {
+  constructor() {
+    this.items = [];
+  }
 
-//   addProductToCart(product) {}
+  addProducto(producto) {
+    this.items.push(producto);
+  }
 
-//   removeProductFromCart(product) {}
+  removeProducto(producto) {}
 
-//   precioTotal() {}
-// }
+  precioTotal() {}
+}
 
 var productos;
+var miCarrito = new Carrito();
 
 window.onload = () => {
   let objFromJSON = JSON.parse(dbProdJSON);
@@ -37,6 +40,11 @@ window.onload = () => {
       object.stock
     );
   });
+
+  // let botonAddCarrito = document.querySelector(`.addCarrito`);
+  // botonAddCarrito.addEventListener("click", () => {
+  //   console.log("agregaste elemento");
+  // });
 
   form.addEventListener("submit", () => {
     event.preventDefault();
@@ -63,45 +71,52 @@ window.onload = () => {
   });
 };
 
-function addToCart(element) {
+function contador(element) {
   let contador = document.getElementById("cartCounter");
-  let contNum = parseInt(contador.innerHTML);
-  contador.innerHTML = contNum + 1;
-  console.log(element);
+  contador.innerHTML = miCarrito.items.length + 1;
 }
 
 function crearTarjeta(element) {
   let contenedor = document.createElement("tr");
-  contenedor.id = element.id;
+  // contenedor.id = element.id;
   let tarjProd = crearComponente("div", "p-2");
   let img = crearComponente("img", "img-fluid rounded shadow-sm");
   img.src = element.imagen;
   img.alt = element.producto;
   img.width = 300;
   let tarjDescr = crearComponente("div", "ml-3 d-inline-block align-middle");
+
   let botones = crearComponente("h5", "mb-0 text-dark d-inline-block");
   botones.innerHTML = element.producto;
+
   let descripcion = crearComponente("p", "descripcion");
   descripcion.innerHTML = element.descripcion;
+
   let td01 = crearComponente("td", "align-middle");
   td01.innerHTML = `<strong>$${element.precio}</strong>`;
+
   let td02 = crearComponente("td", "align-middle");
   td02.innerHTML = `<strong>${element.stock}</strong>`;
+
   let td03 = crearComponente("td", "align-middle");
-  let botonAdd = crearComponente("button", "text-dark");
-  botonAdd.innerHTML = `<i class="fa fa-trash"></i>`;
+  let botonAdd = crearComponente("a", "text-dark");
+  botonAdd.innerHTML = `<i class="fa fa-shopping-basket"></i>`;
   botonAdd.addEventListener("click", () => {
-    addToCart(element);
+    contador(element);
+    miCarrito.addProducto(element);
   });
+
   tarjProd.appendChild(img);
   tarjProd.appendChild(tarjDescr);
   tarjDescr.appendChild(botones);
   tarjDescr.appendChild(descripcion);
   td03.appendChild(botonAdd);
+
   contenedor.appendChild(tarjProd);
   contenedor.appendChild(td01);
   contenedor.appendChild(td02);
   contenedor.appendChild(td03);
+
   return contenedor;
 }
 
@@ -110,6 +125,36 @@ function crearComponente(tag, classes) {
   componente.classList = classes;
   return componente;
 }
+
+const openEls = document.querySelectorAll("[data-open]");
+const closeEls = document.querySelectorAll("[data-close]");
+const isVisible = "is-visible";
+
+for (const el of openEls) {
+  el.addEventListener("click", function () {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+for (const el of closeEls) {
+  el.addEventListener("click", function () {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
+document.addEventListener("click", (e) => {
+  if (e.target == document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
+
+document.addEventListener("keyup", (e) => {
+  // if we press the ESC
+  if (e.key == "Escape" && document.querySelector(".modal.is-visible")) {
+    document.querySelector(".modal.is-visible").classList.remove(isVisible);
+  }
+});
 
 // class ProductOnCart {
 //     constructor(product) {
